624d4e92080d1be197a07e1ea5b58412c6abc02e
star_fullstack <rissatk08@gmail.com>
2021-08-16 12:46:22 +0800

    MySQL Connected

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,12 @@
+import  mysql.connector
 from django.http import HttpResponse
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+)
+print(mydb)
+
 def index(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Hello World! MySQL is connected")
